98d8110afcbcc31d5426c2de8ca3541e819d6cd6
sysecho <707406343@qq.com>
2018-12-20 21:52:10 +0800

    Update ddl.sql

--- insurance/src/main/resources/sql/ddl.sql
+++ insurance/src/main/resources/sql/ddl.sql
@@ -4,5 +4,7 @@ CREATE TABLE `user` (
   `name` varchar(30) DEFAULT NULL COMMENT '姓名',
   `age` int(11) DEFAULT NULL COMMENT '年龄',
   `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
+  `sex` int(2) DEFAULT NULL COMMENT '性别',
+  `is_delete` varchar(2) DEFAULT 'N' COMMENT '是否删除',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
